60ea6b846acedb19fb64e0010e06cbefad9fd17c
pranavpeter2050 <97501797+pranavpeter2050@users.noreply.github.com>
2022-07-08 06:56:52 +0530

    Created DishController and related twig file

---
+++ src/Controller/DishController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Dish;
+use App\Repository\DishRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/dish", name="dish.")
+ */
+
+class DishController extends AbstractController
+{
+    /**
+     * @Route("/", name="edit")
+     */
+    public function index(DishRepository $dishRepository): Response
+    {
+        $dish = $dishRepository->findAll();
+
+        return $this->render('dish/index.html.twig', [
+            'dishes' => $dish,
+        ]);
+    }
+
+    /**
+     * @Route("/create", name="create")
+     */
+    public function create(Request $request) {
+        $dish = new Dish();
+        $dish->setName('Pizza');
+
+        // Entity Manager $em
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($dish);
+        $em->flush();
+
+        // Response
+        return new Response('Dish has been created.');
+    }
+}
