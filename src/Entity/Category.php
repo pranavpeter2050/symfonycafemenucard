8dda641fb6a0443a60ac4e104ee8d3470370cd6d
pranavpeter2050 <97501797+pranavpeter2050@users.noreply.github.com>
2022-07-12 17:38:57 +0530

    created getter/setter functions for doctrine relationship defined between Dish & Category entities

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\CategoryRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -27,7 +29,10 @@ class Category
      */
     private $dish;
 
-    // more on Doctrine Relationships in: symfony.com/doc/current/doctrine/associations.html
+    public function __construct()
+    {
+        $this->dish = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -44,5 +49,40 @@ class Category
         $this->name = $name;
 
         return $this;
+    }
+
+    /**
+     * @return Collection<int, Dish>
+     */
+    public function getDish(): Collection
+    {
+        return $this->dish;
+    }
+
+    public function addDish(Dish $dish): self
+    {
+        if (!$this->dish->contains($dish)) {
+            $this->dish[] = $dish;
+            $dish->setCategory($this);
+        }
+
+        return $this;
+    }
+
+    public function removeDish(Dish $dish): self
+    {
+        if ($this->dish->removeElement($dish)) {
+            // set the owning side to null (unless already changed)
+            if ($dish->getCategory() === $this) {
+                $dish->setCategory(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function __toString() // allows to display a list of objects as a string.
+    {
+        return $this->name;   
     }
 }
--- src/Entity/Dish.php
+++ src/Entity/Dish.php
@@ -94,4 +94,16 @@ class Dish
 
         return $this;
     }
+
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?Category $category): self
+    {
+        $this->category = $category;
+
+        return $this;
+    }
 }
