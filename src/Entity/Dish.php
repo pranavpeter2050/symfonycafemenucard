7f346811bf075ad2ad0852778be33f6df56a12f6
pranavpeter2050 <97501797+pranavpeter2050@users.noreply.github.com>
2022-07-12 10:41:04 +0530

    #file-upload: added "image" column to Dish entity.

--- src/Entity/Dish.php
+++ src/Entity/Dish.php
@@ -21,6 +21,11 @@ class Dish
      * @ORM\Column(type="string", length=255)
      */
     private $name;
+    
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $image;
 
     /**
      * @ORM\Column(type="string", length=255, nullable=true)
@@ -69,6 +74,18 @@ class Dish
     public function setPrice(?float $price): self
     {
         $this->price = $price;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
 
         return $this;
     }
