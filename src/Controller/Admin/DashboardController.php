ba7bf2756bb68731f3f50c76d0eba1d5b8270ec9
pranavpeter2050 <97501797+pranavpeter2050@users.noreply.github.com>
2022-07-23 00:50:57 +0530

    Created new sub-menus and related code change.

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Category;
+use App\Entity\Dish;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,18 +18,24 @@ class DashboardController extends AbstractDashboardController
      */
     public function index(): Response
     {
-        return parent::index();
+        // redirect to some CRUD controller
+        $routeBuilder = $this->get(AdminUrlGenerator::class);
+
+        return $this->redirect($routeBuilder->setController(DishCrudController::class)->generateUrl());
     }
 
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('Symfonycafemenucard');
+            ->setTitle('SymfonyMenuCard Admin');
     }
 
-    public function configureMenuItems(): iterable
+    public function configureMenuItems(): iterable // allows us to further customize the dashboard.
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::section('MenuMap');
+        yield MenuItem::linkToCrud('Dish', 'fa fa-cube', Dish::class);
+        yield MenuItem::linkToCrud('Category', 'fa fa-tasks', Category::class);
         // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
     }
 }
